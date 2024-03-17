6e8a9f716093b6db76a37036024e36436fd82d55
Erthium <ertyum35@gmail.com>
2024-03-17 23:34:38 +0300

    feat: create a basic footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <>
+      <footer>
+        <p>© 2024 Copyright: Duck Tech Dev</p>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
