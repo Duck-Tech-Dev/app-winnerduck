2e39d71e6eb56ac30660ce8443a124d1ea0babe0
Erthium <ertyum35@gmail.com>
2024-03-18 07:59:59 +0300

    chore: remove temporarily used client side auth checking

--- src/app/panel/page.tsx
+++ src/app/panel/page.tsx
@@ -1,23 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
-import { APIService } from "@/services/api";
-import { useRouter } from "next/navigation";
-
 export default function Home() {
-  const router = useRouter();
-  
-  // this part is used temporarily
-  // we need to set up a middleware to check authentication
-  useEffect(() => {
-    const fetchProfile = async () => {
-      const isAuthorized = await APIService.checkConnection();
-      if (!isAuthorized) {
-        router.push("/panel/auth");
-      }
-    }
-    fetchProfile(); 
-  }, [router]);
 
   return (
     <main>
