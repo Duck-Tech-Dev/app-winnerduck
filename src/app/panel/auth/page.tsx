3710f42f5fa9fffc4140cfb96a0d69830b2919c7
Erthium <ertyum35@gmail.com>
2024-03-18 06:59:30 +0300

    style: add some padding to auth card

--- src/app/panel/auth/page.tsx
+++ src/app/panel/auth/page.tsx
@@ -6,8 +6,8 @@ import SignUp from "./SignUp";
 
 export default function Home() {
   return (
-    <div className="flex items-center justify-center ">
-      <Card className="mx-auto max-w-sm space-y-8 flex justify-center align-center">
+    <div className="flex items-center justify-center min-h-screen">
+      <Card className="mx-auto max-w-sm space-y-8 p-8">
         <TabGroup>
           <TabList variant="line" defaultValue="1">
             <Tab value="1">Log In</Tab>
