a4aae31e121d7ea200df917987ca9eb714f90c09
Erthium <ertyum35@gmail.com>
2024-03-25 08:54:11 +0300

    feat: create a component that toggle dark mode

---
+++ src/components/ToggleTheme.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { useState } from 'react';
+import { Switch } from '@tremor/react';
+
+const ToggleTheme = () => {
+  const [isDarkMode, setIsDarkMode] = useState<boolean>(false);
+
+  const toggleTheme = () => {
+    setIsDarkMode(!isDarkMode);
+    document.documentElement.classList.toggle('dark', isDarkMode);
+  };
+
+  return (
+    <Switch 
+      checked={isDarkMode} 
+      onChange={toggleTheme}
+      />
+  );
+};
+
+export default ToggleTheme;
