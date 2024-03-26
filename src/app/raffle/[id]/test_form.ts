ff96dea05b09a3063c5a240610b78bcfa80dab3e
Erthium <ertyum35@gmail.com>
2024-03-26 21:06:56 +0300

    feat: create a test form to be used until backend is ready

---
+++ src/app/raffle/[id]/test_form.ts
@@ -0,0 +1,69 @@
+import { RaffleForm } from "@/interfaces/raffleForm";
+
+export const TestForm: RaffleForm = {
+  id: "10101010",
+  title: "Test Form",
+  description: "This is a test form, Regular Show is crazy maan, yeyyooo",
+  questions: [
+    {
+      title: "What is your name?",
+      description: "Please enter your name",
+      type: "text",
+      template: "text"
+    },
+    {
+      title: "What is your email?",
+      description: "Please enter your email",
+      type: "text",
+      template: "email"
+    },
+    {
+      title: "What is your age?",
+      description: "Please enter your age",
+      type: "number",
+      template: "number"
+    },
+    {
+      title: "What is your favorite color?",
+      description: "Please enter your favorite color",
+      type: "text",
+      template: "text"
+    },
+    {
+      title: "What is your favorite food?",
+      description: "Please enter your favorite food",
+      type: "text",
+      template: "text"
+    },
+    {
+      title: "What is your favorite movie?",
+      description: "Please enter your favorite movie",
+      type: "text",
+      template: "text"
+    },
+    {
+      title: "What is your favorite song?",
+      description: "Please enter your favorite song",
+      type: "text",
+      template: "text"
+    },
+    {
+      title: "What is your favorite book?",
+      description: "Please enter your favorite book",
+      type: "text",
+      template: "text"
+    },
+    {
+      title: "What is your favorite game?",
+      description: "Please enter your favorite game",
+      type: "text",
+      template: "text"
+    },
+    {
+      title: "What is your favorite animal?",
+      description: "Please enter your favorite animal",
+      type: "text",
+      template: "text"
+    },
+  ]
+};
