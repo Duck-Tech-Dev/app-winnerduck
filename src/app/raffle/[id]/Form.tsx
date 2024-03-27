3bfe6e56d7da7bc3ff98ddc0f0280313f10c2693
Erthium <ertyum35@gmail.com>
2024-03-27 18:55:40 +0300

    feat: create a mechanism that questions validate input and send the output to form

--- src/app/raffle/[id]/Form.tsx
+++ src/app/raffle/[id]/Form.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React from 'react';
+import React, { useState } from 'react';
 import { RaffleForm } from "@/interfaces/raffleForm";
 import Question from './Question';
 import { Card, Button } from '@tremor/react';
@@ -9,18 +9,39 @@ import { useEffect } from 'react';
 
 interface FormProps {
   raffleForm: RaffleForm;
-  sendForm: () => void;
+  handleSendForm: () => void;
 }
 
-const Form: React.FC<FormProps> = ({ raffleForm, sendForm }) => {
-  const submitForm = () => {
-    sendForm();
-    // ...APIService.sendForm... and stuff
+const Form: React.FC<FormProps> = ({ raffleForm, handleSendForm }) => {
+  const [fields, setFields] = useState<Array<string | null>>([null,]);
+  const [warningMessage, setWarningMessage] = useState<string | null>(null);
+
+  useEffect(() => {
+    // if the field is required, set it to null
+    const initialFields = raffleForm.questions.map(question => question.isRequired ? null : '');
+    setFields(initialFields);
+  }, [raffleForm.questions]);
+
+  const onFieldChecked = (index: number, newValue: string | null) => {
+    const newFields = [...fields];
+    newFields[index] = newValue;
+    setFields(newFields);
+  }
+
+  const submitForm = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    // check if all necessary fields are filled
+    if (fields.includes(null)) {
+      setWarningMessage('Please fill in all required fields');
+      return;
+    }
+    handleSendForm();
+
   }
 
   return (
     <Card className="max-w-lg mx-4 my-12 p-8">
-      <form>
+      <form onSubmit={submitForm}>
         <h2 className="mb-3 text-center text-2xl font-semibold text-tremor-content-strong dark:text-dark-tremor-content-strong">
           {raffleForm.title}
         </h2>
@@ -29,13 +50,17 @@ const Form: React.FC<FormProps> = ({ raffleForm, sendForm }) => {
         </p>
         {raffleForm.questions.map((question, index) => (
           <div key={index} className="my-6">
-            <Question questionInfo={question}/>
+            <Question index={index} questionInfo={question} onFieldChecked={onFieldChecked}/>
           </div>
         ))}
+        {warningMessage && (
+          <p className="text-red-500">{warningMessage}</p>
+        )}
         <Button
           className="my-3 w-full" 
           variant="primary"
-          onClick={submitForm}>
+          type="submit"
+        >
           Submit
         </Button>
       </form>
--- src/app/raffle/[id]/Question.tsx
+++ src/app/raffle/[id]/Question.tsx
@@ -2,40 +2,101 @@
 
 import React from 'react';
 import { Question } from "@/interfaces/raffleForm";
-import { TextInput, NumberInput } from '@tremor/react';
+import { Card, TextInput, NumberInput } from '@tremor/react';
 import { useEffect } from 'react';
 
 interface QuestionProps {
+  index: number;
   questionInfo: Question;
+  onFieldChecked: (index: number, newValue: string | null) => void;
 }
 
-const Question: React.FC<QuestionProps> = ({ questionInfo }) => {
+const getQuestionRegex = (template: string): RegExp => {
+  // GIANT TODO: Currently this function is created by 
+  // copilot for testing, will be worked on later
+  switch (template) {
+    case "email":
+      return /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    case "phone":
+      return /^\d{10}$/;
+    case "number":
+      return /^\d+$/;
+    case "text":
+      return /./; // sould be removed from special characters that might become dangerous
+    default:
+      return /./;
+  }
+}
+
+const Question: React.FC<QuestionProps> = ({ index, questionInfo, onFieldChecked }) => {
+  const [fieldError, setFieldError] = React.useState<string | null>(null);
+  const questionRegex = getQuestionRegex(questionInfo.template);
+
+  const isFieldValid = async (currentValue: string): Promise<boolean> => {
+    if (!questionInfo.isRequired) {
+      setFieldError(null);
+      return true;
+    }
+    if (currentValue === '') {
+      setFieldError('This field is required');
+      return false
+    }
+    if (!questionRegex.test(currentValue)) {
+      setFieldError('Invalid input');
+      return false;
+    }
+    setFieldError(null);
+    return true;
+  }
+
+  const handleInputValueChanged = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const isValid = await isFieldValid(e.target.value);
+    if (!isValid) {
+      onFieldChecked(index, null);
+    }
+    else {
+      onFieldChecked(index, e.target.value);
+    }
+  }
 
   const inputField = () => {
     if (questionInfo.type === "text") {
       return (
-        <TextInput/>
+        <TextInput onChange={handleInputValueChanged}/>
       );
     }
     if (questionInfo.type === "number") {
       return (
-        <NumberInput enableStepper={false}/>
+        <NumberInput enableStepper={false} onChange={handleInputValueChanged}/>
       );
     }
   }
 
   return (
-    <>
-      <h3 className='my-3 text-lg text-center text-tremor-content-strong dark:text-dark-tremor-content-strong'>
-        {questionInfo.title}
-      </h3>
-      <p className='my-3 text-sm text-center text-tremor-content-strong dark:text-dark-tremor-content-strong'>
+    <Card 
+      className="max-w-lg mx-4 my-8 p-4"
+      decoration="left"
+      >
+      <div className="flex">
+        <h3 className='mb-3 text-lg text-tremor-content-strong dark:text-dark-tremor-content-strong'>
+          {questionInfo.title}
+        </h3>
+        {questionInfo.isRequired && (
+          <p className='text-red-500 ml-2'>*</p>
+        )}
+      </div>
+      <p className='mb-3 text-sm text-tremor-content-strong dark:text-dark-tremor-content-strong'>
         {questionInfo.description}
       </p>
       <div className='my-3'>
         {inputField()}
       </div>
-    </>
+      {fieldError && (
+        <p className="text-red-500 my-3">
+          {fieldError}
+        </p>
+      )}
+    </Card>
   );
 }
 
