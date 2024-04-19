9355894d4bedf2a69944066b03480ce90554d628
Erthium <ertyum35@gmail.com>
2024-04-19 23:05:46 +0300

    feat: organise new question types

--- src/app/raffle/[id]/Question.tsx
+++ src/app/raffle/[id]/Question.tsx
@@ -75,37 +75,51 @@ const Question: React.FC<QuestionProps> = ({ index, questionInfo, onFieldChecked
   const inputField = () => {
     if (questionInfo.type === "text") {
       return (
-        <TextInput onChange={handleInputChanged}/>
+        <TextInput 
+          className="duration-0" 
+          onChange={handleInputChanged}/>
       );
     }
     if (questionInfo.type === "email") {
       return (
         <TextInput 
+          className="duration-0" 
           onChange={handleInputChanged} 
-          placeholder="example@duck.quack"/>
+          placeholder="quack@winner.duck"/>
       );
     }
     if (questionInfo.type === "number") {
       return (
-        <NumberInput 
-            enableStepper={false} 
-            onChange={handleInputChanged} 
-            min={questionInfo.minimum} 
-            max={questionInfo.maximum}/>
+        <NumberInput
+          className="duration-0"
+          enableStepper={false} 
+          onChange={handleInputChanged} 
+          min={questionInfo.minimum} 
+          max={questionInfo.maximum}/>
       );
     }
     if (questionInfo.type === "phone") {
       return (
-          <NumberInput
-            onChange={handleInputChanged} 
-            enableStepper={false} 
-            max={9999999999}
-            placeholder="5554443322"/>
+          <div className="flex">
+            <select className="flex w-auto outline-none w-full outline-none text-left whitespace-nowrap truncate rounded-tremor-default focus:ring-2 transition duration-100 border pr-8 py-2 border-tremor-border shadow-tremor-input focus:border-tremor-brand-subtle focus:ring-tremor-brand-muted dark:border-dark-tremor-border dark:shadow-dark-tremor-input dark:focus:border-dark-tremor-brand-subtle dark:focus:ring-dark-tremor-brand-muted bg-tremor-background border-tremor-border divide-tremor-border shadow-tremor-dropdown dark:bg-dark-tremor-background dark:border-dark-tremor-border dark:divide-dark-tremor-border dark:shadow-dark-tremor-dropdown text-tremor-content-subtle dark:text-dark-tremor-content-subtle">
+              <option value="+90" className="flex justify-start items-center cursor-default text-tremor-default px-2.5 py-2.5 ui-active:bg-tremor-background-muted  ui-active:text-tremor-content-strong ui-selected:text-tremor-content-strong ui-selected:bg-tremor-background-muted text-tremor-content-emphasis dark:ui-active:bg-dark-tremor-background-muted  dark:ui-active:text-dark-tremor-content-strong dark:ui-selected:text-dark-tremor-content-strong dark:ui-selected:bg-dark-tremor-background-muted dark:text-dark-tremor-content-emphasis">
+                +90
+              </option>
+            </select>
+            <NumberInput
+              className="duration-0" 
+              onChange={handleInputChanged} 
+              enableStepper={false} 
+              max={9999999999}
+              placeholder="5554443322"/>
+          </div>
       );
     }
     if (questionInfo.type === "radio") {
       return (
-        <Select onChange={handleInputValueChanged}>
+        <Select 
+          className="duration-0" 
+          onChange={handleInputValueChanged}>
           {questionInfo.options?.map((option, index) => (
             <SelectItem key={index} value={option}>
               {option}
@@ -116,7 +130,9 @@ const Question: React.FC<QuestionProps> = ({ index, questionInfo, onFieldChecked
     }
     if (questionInfo.type === "checkbox") {
       return (
-        <MultiSelect onChange={handleInputValueChanged}>
+        <MultiSelect 
+          className="duration-0" 
+          onChange={handleInputValueChanged}>
           {questionInfo.options?.map((option, index) => (
             <MultiSelectItem key={index} value={option}>
               {option}
@@ -148,7 +164,9 @@ const Question: React.FC<QuestionProps> = ({ index, questionInfo, onFieldChecked
     }
     if (questionInfo.type === "date") {
       return (
-        <DatePicker onChange={handleInputChanged}/>
+        <DatePicker 
+          className="duration-0" 
+          onChange={handleInputChanged}/>
       );
     }
   }
